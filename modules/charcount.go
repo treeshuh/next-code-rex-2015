c540e5f62db7db2e10abc9e85ae70bb6cdeec7a0
Michael Xu <michaelx@michaelx-9470m.(none)>
2013-12-01 15:33:17 -0500

    now counting string literals in charcount.go

--- modules/charcount.go
+++ modules/charcount.go
@@ -7,21 +7,41 @@ import (
        "regexp"
 )
 
+func countLiteral(s string) (int, string) {
+    p, _ := regexp.Compile("(?s)((''').*?('''))|(\".*?\")|('[^']*?')")
+    match := p.FindStringIndex(s)
+    if match != nil {
+        return match[1] - match[0], s[:match[0]] + s[match[1]:]
+    }
+    return 0, s
+}
+
 func main() {
 
     content, err := ioutil.ReadAll(os.Stdin)
     if err != nil {
         fmt.Println("Error while reading from stdin.")
     }
-    line := string(content)
+    source := string(content)
+
+    charCount := 0
+
+    for true {
+        count, newstr := countLiteral(source)
+        if count == 0 {
+            break
+        }
+        charCount += count
+        source = newstr
+    }
 
     // Replace identifiers with '?'
     ireg, _ := regexp.Compile("[A-Za-z0-9]+")
-    line = ireg.ReplaceAllString(line,"?")
+    source = ireg.ReplaceAllString(source,"?")
 
     // Remove all whitespace
     sreg, _ := regexp.Compile("\\s+")
-    line = sreg.ReplaceAllString(line,"")
+    source = sreg.ReplaceAllString(source,"")
 
-    fmt.Printf("%d\n",len(line))
+    fmt.Printf("%d\n",len(source) + charCount)
 }
