5b2df0a2d191c30f55809dc4b801140f76844898
treeshuh <tricia.shi@gmail.com>
2015-07-21 00:05:18 -0400

    speed css

--- public/javascripts/speed.js
+++ public/javascripts/speed.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
 	var viewmodel = function(){
 		var maxTime = timeLimit;
-		var startTime; var timeIntervalId;
+		var startTime;
 		var targetWords = targetString.split(" ");
 		var index = ko.observable(0);
 	 	var targetText = ko.observable(targetWords[0]);
 		var inputText = ko.observable("");
 		var challenging = ko.observable(false);
-		var alertMessage = ko.observable("");
+		var maxTimerWidth = $(".timer").width();
+		var oneSegmentWidth = String(maxTimerWidth/targetWords.length) + "px";
+
+		if (previousScore) {
+			alertSuccess("You have already completed this challenge.", "");
+			$("#max-score").addClass("max");
+		}
 
 		var valid = ko.computed(function(){
 			if (index() == targetWords.length - 1 && targetText() == inputText()) {
-				$(".input-progress").css("width", "200px");
-				window.clearInterval(timeIntervalId);
+				$(".input-progress").animate({"width": maxTimerWidth});
+				$(".timer-progress").stop();
+				challenging(false);
+				index(0);
 				$.ajax({
 			        type: "POST",
 			        url: "/submit",
@@ -25,8 +33,8 @@ $(document).ready(function(){
 			            }
 			        },
 			        success: function(data) {
-			            console.log(data.result)
-			            handleResult(data.result);
+			            $("#max-score").addClass("max");
+			            alertSuccess("Success!", "You passed the speed challenge.")
 			        }
 			    })
 				return true
@@ -34,42 +42,32 @@ $(document).ready(function(){
 			else if (inputText()[inputText().length - 1] == " " && targetText() == inputText().substring(0, inputText().length-1)){
 				//valid input
 				index(index()+1);
-				$(".input-progress").css("width", ""+ 200*index()/targetWords.length + "px");
+				$(".input-progress").animate({"width": "+=" + oneSegmentWidth});
 				if (index() != targetWords.length){
 					targetText(targetWords[index()]);	
 					inputText("")
-				}
-				else{
 				}
 			}
 			return false
 		})
 
 		var startChallenge = function(){
-			alertMessage("");
 			challenging(true);
 		 	setUpTimer();
 		 	$("input[type=text]").focus();
+		 	$(".timer-progress").css("width", "0");
+		 	$(".input-progress").css("width", "0");
 		}
 
 		var setUpTimer = function(){
 			startTime = Date.now();
-			time = 0;
-			timeIntervalId = setInterval(function(){
-				time += 500;
-				if(time >= maxTime*1000 && index() > 0){
-					window.clearInterval(timeIntervalId);
-					window.alert("fail");
-				}
-				$(".timer-progress").css("width", ""+ 200*time/(maxTime*1000) + "px");
-			}, 500)
-		}
-
-		function handleResult(result) {
-			text = "Your score is " + result.score + ".";
-			text += "This is " + String(result.score - result.previousScore) + " more than your previous score";
-			console.log(text);
-			alertMessage(text);
+			$(".timer-progress").animate({
+				"width": "100%"
+			}, maxTime*1000, "linear", function(){
+				alertError("You ran out of time!", "Press START to try again.");
+				index(0);
+				challenging(false);
+			})
 		}
 
 		return{
@@ -80,8 +78,19 @@ $(document).ready(function(){
 			targetWords: targetWords,
 			index: index,
 			startChallenge: startChallenge,
-			alertMessage: alertMessage
 		}
 	}
 	ko.applyBindings(viewmodel())	
 })
+
+function alertError(header, message) {
+    error = '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+    error += "<strong>" + header + "</strong>\t" + message + "</div>";
+    $("#alerts").append(error)
+}
+
+function alertSuccess(header, message) {
+    success = '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+    success += "<strong>" + header + "</strong>\t" + message + "</div>";
+    $("#alerts").append(success)
+}
